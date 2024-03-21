b59a92e74905a426675773a0d8d7398e1c602479
RichDi <drdr_2@hotmail.com>
2024-03-21 16:30:04 -0600

    Estructura base de la aplicación

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './platzi.webp';
+import { TodoCounter } from './TodoCounter.js'
+import { TodoSearch } from './TodoSearch.js'
+import { TodoList } from './TodoList.js'
+import { TodoItem } from './TodoItem.js';
+import { CreateTodoButton } from './CreateTodoButton.js';
 import './App.css';
+
+const defaultTodos = [
+  { text: 'Cortar cebolla', completed: true },
+  { text: 'Tomar el curso de intro a React', completed: false },
+  { text: 'Llorar con la llorona', completed: false },
+  { text: 'LALALALA', completed: false },
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TodoCounter
+        total={10}
+        completed={3}
+      />
+      <TodoSearch />
+      <TodoList>
+        {defaultTodos.map(todo => (
+          <TodoItem 
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+          />
+        ))}
+      </TodoList>
+
+      <CreateTodoButton /> 
+    </>
   );
 }
-
 export default App;
